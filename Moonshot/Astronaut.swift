dfa73a9971f8c400593b1b11cd043e9cdf8f0d9f
Promal B <promal.barua.cse@gmail.com>
2021-07-27 04:32:51 +0600

    Project is functional. Will need to review the challenges and add some animation later on.

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Promal on 27/7/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
+
